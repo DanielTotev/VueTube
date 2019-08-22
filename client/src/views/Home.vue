dfa9e4da6d814b52c9178187261248e279ff00e3
DanielTotev <dtotev1@gmail.com>
2019-08-22 11:59:51 +0300

    Renamed a component

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -7,7 +7,7 @@
     <div class="container">
 			<div class="row">
 				<div class="col col-md-4"  v-for="video in videos" :key="video._id">
-					<VideoHomePage :video="video" />
+					<VideoHomePagePartial :video="video" />
 				</div>
 			</div>
     </div>
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-import VideoHomePage from "@/components/video/VideoHomePage"
+import VideoHomePagePartial from "@/components/video/VideoHomePagePartial"
 import { getVideos } from "./../mixins/services/videoService";
 
 export default {
@@ -26,7 +26,7 @@ export default {
 		}
   },
   components: {
-    VideoHomePage
+    VideoHomePagePartial
   },
 	mixins: [getVideos],
 	created() {
