44ed6eb48ab1ea65ec3e9e9a9c86396ba83250a5
DanielTotev <dtotev1@gmail.com>
2019-08-22 16:08:55 +0300

    Added some settings

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = 'mongodb://localhost:27017/vueTube';
+const db = process.env.NODE_ENV == 'production'? process.env.db_url : 'mongodb://localhost:27017/vueTube';
 
 module.exports = () => {
     mongoose.connect(db, {useNewUrlParser : true})
